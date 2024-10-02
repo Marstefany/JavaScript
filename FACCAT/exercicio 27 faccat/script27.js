18fda73be9709eaa9dd0ce1d1f60b53250b649ae
Marstefany <mariastefany008@gmail.com>
2024-10-01 22:05:25 -0300

    first commit

---
+++ FACCAT/exercicio 27 faccat/script27.js
@@ -0,0 +1,16 @@
+/*Ler um valor e escrever se é positivo, negativo ou zero.*/
+
+
+
+let numero = Number(prompt("Digite um numero: "))
+if (numero < 0){
+    alert("O numero " + numero + " é negativo")
+}else{
+
+}if (numero > 0){
+    alert("O numero " + numero + " é positivo")
+}else{
+
+}if (numero = 0){
+    alert("O numero " + numero + " é igual a zero")
+}else{}
